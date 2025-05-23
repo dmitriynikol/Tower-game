b79fe0aeabbba1f56df36a5fb04a8b8c0702824e
fvbrikoshin <fedorbrikgcr@gmail.com>
2025-05-24 02:35:23 +0300

    Add files via upload

---
+++ maps/level1/tileset.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="block_tileset" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../../img/tiles/brick.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../../img/tiles/spikeup.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../../img/tiles/spikedown.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../../img/tiles/spikeleft.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../../img/tiles/spikeright.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../../img/tiles/illusion.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../../img/tiles/crack.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../../img/tiles/grass.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../../img/tiles/box.png"/>
+ </tile>
+</tileset>
